edf9c23af8eb4b0375eebda2a7600c99be3b7191
grace866 <grtan@seas.upenn.edu>
2025-04-12 15:55:54 -0400

    commit

--- Assets/Overbrewed/Overbrewed/Scripts/PlayerScripts/PlayerMovementScript.cs
+++ Assets/Overbrewed/Overbrewed/Scripts/PlayerScripts/PlayerMovementScript.cs
@@ -4,55 +4,67 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
-    Rigidbody rb;
-    CapsuleCollider col;
+
+    Animator animator;
+    CharacterController controller;
+
     public Transform playerBody;
 
     [Header("Movement")]
     public float speed = 3f;
-    public float acceleration = 12f, decelerationFactor = 1f; 
+    public float acceleration = 12f;
+    public float decelerationFactor = 1f;
+    public float gravity = -9.81f;
+
+    private Vector3 velocity;
 
     private void Start() {
-        rb = playerBody.GetComponent<Rigidbody>();
-        col = playerBody.GetComponent<CapsuleCollider>();
+        controller = playerBody.GetComponent<CharacterController>();
+        animator = playerBody.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update() {
         Walk();
     }
+
     void Walk()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
         float maxSpeed = speed, maxAcc = acceleration;
 
-        float horizonalInput = Input.GetAxis("Horizontal");
-        float veritcalInput = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(horizonalInput, 0, veritcalInput);
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+  
         float len = direction.magnitude;
+        bool isMoving = len > 0.1f;
 
-        if (len > 0)
+        // Update Animator parameter
+        if (animator != null)
         {
-            rb.velocity += direction / len * Time.deltaTime * maxAcc;
+            animator.SetBool("isWalking", isMoving);
+        }
 
-            // Clamp velocity to the maximum speed.
-            if (rb.velocity.magnitude > maxSpeed)
-            {
-                rb.velocity = rb.velocity.normalized * speed;
-            }
-            if (direction != Vector3.zero) {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // small downward force to keep grounded
+        }
+
+        if (isMoving)
+        {
+            Vector3 moveDir = direction * speed;
+            controller.Move(moveDir * Time.deltaTime);
+
+            // Face movement direction
+            if (direction != Vector3.zero)
                 playerBody.forward = direction;
-            }
         }
-        else
-        {
-            // If no buttons are pressed, decelerate.
-            len = rb.velocity.magnitude;
-            float decel = acceleration * decelerationFactor * Time.deltaTime;
-            if (len < decel) rb.velocity = Vector3.zero;
-            else
-            {
-                rb.velocity -= rb.velocity.normalized * decel;
-            }
-        }
+
+        // Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
     }
 }
