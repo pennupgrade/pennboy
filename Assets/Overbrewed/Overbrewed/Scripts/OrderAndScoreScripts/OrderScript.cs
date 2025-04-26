58dd39e61d30eba41d26d92021e92a7f294d784d
grace866 <grtan@seas.upenn.edu>
2025-04-26 15:51:12 -0400

    commit

--- Assets/Overbrewed/Overbrewed/Scripts/OrderAndScoreScripts/OrderScript.cs
+++ Assets/Overbrewed/Overbrewed/Scripts/OrderAndScoreScripts/OrderScript.cs
@@ -12,7 +12,7 @@ public class OrderScript : MonoBehaviour
     private List<int> currentServed;
     public ScoreScript score;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         orders = new Queue<List<int>>();
         milkAmt = new List<int>() {0, 1, 2, 3};
