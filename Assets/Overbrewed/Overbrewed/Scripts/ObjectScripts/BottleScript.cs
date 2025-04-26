75398fcc4802ebbaf0971fc1493f2e0bccc89505
Gracie Y <gracie.m.yang@gmail.com>
2025-04-26 12:23:20 -0400

    commit

--- Assets/Overbrewed/Overbrewed/Scripts/ObjectScripts/BottleScript.cs
+++ Assets/Overbrewed/Overbrewed/Scripts/ObjectScripts/BottleScript.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class BottleScript : MonoBehaviour
 {
+    public GameObject player;
+    public ButtonAttribute button;
+    private bool isInRange = false;
+    public MugScript mug;
+    public int milkAmount = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
+        button.buttonName = "Oat Milk";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isInRange && mug.mugHeld != null)
+        {
+        }
+    }
+}
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void OnButtonClicked()
+    {
+        Debug.Log("Button was clicked!");
     }
 }
