f7b337519c1ec6d41fb14aa0473cf82f1c169bd3
grace866 <grtan@seas.upenn.edu>
2025-04-26 14:54:52 -0400

    commit

--- Assets/Overbrewed/Overbrewed/Scripts/ObjectScripts/CounterScript.cs
+++ Assets/Overbrewed/Overbrewed/Scripts/ObjectScripts/CounterScript.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 using TMPro; 
 
 public class CounterScript : MonoBehaviour
-//IInteractable
 {
     public GameObject player;
-    private OrderScript orders;
+    public OrderScript orders;
     public TextMeshProUGUI currOrder;
     public List<string> milkTypes;
     private int amt;
@@ -17,56 +16,30 @@ public class CounterScript : MonoBehaviour
     private void Start()
     {
         player = GameObject.FindWithTag("Player");
-        orders = FindObjectOfType<OrderScript>();
+        //orders = FindObjectOfType<OrderScript>();
         milkTypes = orders.milkType;
+        Debug.Log("Next Order: Amt = " + amt + ", Type Index = " + (orders.GetCurrOrder())[1]);
         amt = (orders.GetCurrOrder())[0];
         type = milkTypes[(orders.GetCurrOrder())[1]];
         currOrder.text = "Current Order: \n Milk:" + type + "\n MilkAmt: " + amt;
     }
-
-
-
-    /*private void Start() {
-         player = GameObject.FindWithTag("Player");
-         order = FindObjectOfType<OrderScript>();
-     }
-
-     public void Update()
-     {
-
-     }
-     public void Interact() {
-         if (player != null)
-         {
-             PlayerItemHold holdItem = player.GetComponent<PlayerItemHold>();
-             if (holdItem != null)
-             {
-                 ItemScript item = holdItem.GetItemHeld();
-                 if (item != null) {
-                     if (item.IsServable) {
-                         return;
-                     }
-                 }
-             }
-         }
-     }
-
-     public void Serve() {
-         return;
-     }*/
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Mug"))
         {
             MugScript mug = other.gameObject.GetComponent<MugScript>();
-            List<int> contents = mug.contents; 
+            List<int> contents = mug.contents;
+
             List<int> lastOrder = orders.GetCurrOrder(); // save completed order
+
             AssignPoints(CheckOrder(lastOrder, contents));
+
             orders.OntoNextOrder(); // dequeue completed order
             amt = (orders.GetCurrOrder())[0]; // amt for next order
             type = milkTypes[(orders.GetCurrOrder())[1]]; // type for next order
-            currOrder.text = currOrder.text = "Current Order: \n Milk:" + type + "\n MilkAmt: " + amt; // updated display
+            currOrder.text = "Current Order: \n Milk:" + type + "\n MilkAmt: " + amt; // updated display
+
             Debug.Log("Mug placed on the counter!");
         }
     }
