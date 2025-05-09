662ea36720ec91ffaef983fcf7e1ad52dff8f8bf
grace866 <grtan@seas.upenn.edu>
2025-05-09 09:49:45 -0400

    commit

--- Assets/Overbrewed/Overbrewed/Scripts/UIScripts/uiscript.cs
+++ Assets/Overbrewed/Overbrewed/Scripts/UIScripts/uiscript.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class uiscript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI currOrder;
+    public void updateText(string type, int amt)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        currOrder.text = "Current Order: \n Milk:" + type + "\n MilkAmt: " + amt;
     }
 }
